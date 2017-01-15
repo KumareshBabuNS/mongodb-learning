171729604364cec8c2e157941e0da4d834b9182d
Maciej Szarlinski <mszarlinski@gmail.com>
2017-01-15 17:58:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MongoDB
+
+## Features
+ 
+* Document data model
+* Schemaless
+* Horizontal scaling
+* Indexes
+* JavaScript shell
+ 
+## Command-line tools
+ 
+* mongodump, mongorestore
+* mongoexport, mongoimport
+* mongosniff
+* mongostat
+* mongotop
+* mongoperf
+* mongooplog
+* Bsondump
---
+++ src/main/java/com/github/mszarlinski/mongodb/MongodbLearningApplication.java
@@ -0,0 +1,12 @@
+package com.github.mszarlinski.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbLearningApplication.class, args);
+	}
+}
